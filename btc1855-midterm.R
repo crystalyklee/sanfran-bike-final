3a9451e4d5d1b8adec6e589905b5bcf1d19570aa
Crystal Lee <crystallee@Crystals-MacBook-Pro.local>
2024-07-30 16:20:13 -0400

    Check for duplicates

--- btc1855-midterm.R
+++ btc1855-midterm.R
@@ -1,3 +1,20 @@
 # BTC1855 - CODING IN R
 # MIDTERM PROJECT
 # CRYSTAL LEE
+
+#' Any trip starting and ending at the same station, 
+#' with duration less than 3 minutes is likely a 'cancelled trip'.
+#' Find out the number of such trips, record the trip ids for 
+#' your report and then remove them from the dataset.
+
+stationdata <- read.csv('station.csv')
+tripdata <-read.csv('trip.csv')
+weatherdata <- read.csv('weather.csv')
+
+# Check for duplicates 
+any(duplicated(stationdata))
+any(duplicated(tripdata))
+any(duplicated(weatherdata))
+
+
+
