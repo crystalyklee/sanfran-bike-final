8b24491a49edc87c3c2e4956f78f4593d8c86072
Crystal Lee <crystallee@Crystals-MacBook-Pro.local>
2024-07-31 15:57:33 -0400

    Included code for EDA of three datasets

--- btc1855-midterm.R
+++ btc1855-midterm.R
@@ -2,19 +2,89 @@
 # MIDTERM PROJECT
 # CRYSTAL LEE
 
-#' Any trip starting and ending at the same station, 
-#' with duration less than 3 minutes is likely a 'cancelled trip'.
-#' Find out the number of such trips, record the trip ids for 
-#' your report and then remove them from the dataset.
-
 stationdata <- read.csv('station.csv')
 tripdata <-read.csv('trip.csv')
 weatherdata <- read.csv('weather.csv')
 
-# Check for duplicates 
+# Clean stationdata before analysis
+summary(stationdata)
+
+# Check for duplicates
 any(duplicated(stationdata))
+
+# Clean tripdata before analysis
+summary(tripdata)
+# Check for duplicates
+
 any(duplicated(tripdata))
+# Separate date and time from datetime column
+
+tripdata2 <- tripdata %>% 
+  separate(start_date, c('start_date', 'start_time'), sep = " ") %>% 
+  separate(end_date, c('end_date', 'end_time'), sep = " ") 
+  
+# Change dates into Date class
+tripdata2$start_date <- mdy(tripdata2$start_date)
+tripdata2$end_date <- mdy(tripdata2$end_date)
+
+# Change blank entries into NAs
+tripdata2[tripdata2 == ""] <- NA
+
+# Clean weatherdata before analysis
+summary(weatherdata)
+
+# Check for duplicates
 any(duplicated(weatherdata))
 
+# Change date into Date class
+weatherdata$date <- mdy(weatherdata$date)
+weatherdata[weatherdata == ""] <- NA
 
+# EXPLORATORY DATA ANALYSIS
+
+install.packages("funModeling")
+install.packages("Hmisc")
+library(funModeling) 
+library(tidyverse) 
+library(Hmisc)
+
+# EDA on stationdata
+station_eda <- function(stationdata)
+{
+  glimpse(stationdata)
+  print(status(stationdata))
+  freq(stationdata) 
+  print(profiling_num(stationdata))
+  plot_num(stationdata)
+  describe(stationdata)
+}
+
+station_eda(stationdata)
+
+# EDA on tripdata
+trip_eda2 <- function(tripdata2)
+{
+  glimpse(tripdata2)
+  print(status(tripdata2))
+  freq(tripdata2) 
+  print(profiling_num(tripdata2))
+  plot_num(tripdata2)
+  describe(tripdata2)
+}
+
+trip_eda(tripdata2)
+
+
+# EDA on weatherdata
+weather_eda <- function(weatherdata)
+{
+  glimpse(weatherdata)
+  print(status(weatherdata))
+  freq(weatherdata) 
+  print(profiling_num(weatherdata))
+  plot_num(weatherdata)
+  describe(weatherdata)
+}
+
+trip_eda(weatherdata)
 
