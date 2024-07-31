0377dbb5b73a54bccc444a488d237682e80cca20
Crystal Lee <crystallee@Crystals-MacBook-Pro.local>
2024-07-31 15:49:26 -0400

    Update comments on cleaning

--- btc1855-midterm.R
+++ btc1855-midterm.R
@@ -2,19 +2,41 @@
 # MIDTERM PROJECT
 # CRYSTAL LEE
 
-#' Any trip starting and ending at the same station, 
-#' with duration less than 3 minutes is likely a 'cancelled trip'.
-#' Find out the number of such trips, record the trip ids for 
-#' your report and then remove them from the dataset.
-
 stationdata <- read.csv('station.csv')
 tripdata <-read.csv('trip.csv')
 weatherdata <- read.csv('weather.csv')
 
-# Check for duplicates 
+# Clean stationdata before analysis
+summary(stationdata)
+
+# Check for duplicates
 any(duplicated(stationdata))
+
+# Clean tripdata before analysis
+summary(tripdata)
+# Check for duplicates
+
 any(duplicated(tripdata))
+# Separate date and time from datetime column
+
+tripdata2 <- tripdata %>% 
+  separate(start_date, c('start_date', 'start_time'), sep = " ") %>% 
+  separate(end_date, c('end_date', 'end_time'), sep = " ") 
+  
+# Change dates into Date class
+tripdata2$start_date <- mdy(tripdata2$start_date)
+tripdata2$end_date <- mdy(tripdata2$end_date)
+
+# Change blank entries into NAs
+tripdata2[tripdata2 == ""] <- NA
+
+# Clean weatherdata before analysis
+summary(weatherdata)
+
+# Check for duplicates
 any(duplicated(weatherdata))
 
-
+# Change date into Date class
+weatherdata$date <- mdy(weatherdata$date)
+weatherdata[weatherdata == ""] <- NA
 
